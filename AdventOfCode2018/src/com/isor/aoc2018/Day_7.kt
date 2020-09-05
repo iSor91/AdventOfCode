788bf2ae450dc74b7f6926b1a8a8c65cb8a377cc
isör <ratkovszki.istvn@gmail.com>
2020-09-05 18:07:10 +0200

    Adds day 7 2018

---
+++ AdventOfCode2018/src/com/isor/aoc2018/Day_7.kt
@@ -0,0 +1,21 @@
+package com.isor.aoc2018
+
+import com.isor.aoc.common.AOC_Runner
+import com.isor.aoc.common.Year
+
+fun main() {
+    Day7().executeGoals()
+}
+
+@Year(2018)
+class Day7 : AOC_Runner() {
+
+    override fun executeGoal_1() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun executeGoal_2() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
