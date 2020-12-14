d829cfb36ff9fabee870a41a52b49b47f471f61f
isör <istvan.ratkovszki@loxon.eu>
2020-12-14 08:04:16 +0100

    commits day 14 2020 cleanup

--- AdventOfCode2020/src/Day14.kt
+++ AdventOfCode2020/src/Day14.kt
@@ -1,5 +1,6 @@
 import com.isor.aoc.common.AOC_Runner
 import com.isor.aoc.common.Year
+import kotlin.reflect.KFunction0
 
 fun main() {
     Day14().executeGoals()
@@ -15,86 +16,76 @@ class Day14 : AOC_Runner() {
     val maskRegex = Regex("mask = ([0,1,X]{36})")
     val memRegex = Regex("mem\\[(\\d+)\\] = ([\\d]+)")
 
+    val memory1 = mutableMapOf<Long, String>()
+    val memory2 = mutableMapOf<Long, String>()
+
+    var memAddress: String = ""
+    var toWrite: String = ""
+
     override fun executeGoal_1() {
-        val memory = mutableMapOf<Int, String>()
-
-        allLines.forEach {
-            val maskMatch = maskRegex.matches(it)
-            val memMatch = memRegex.matches(it)
-
-            if(maskMatch) {
-                mask = maskRegex.find(it)!!.groupValues[1]
-            } else {
-                val memValues = memRegex.find(it)!!.groupValues
-                val memAddress = memValues[1]
-                val toWrite = memValues[2]
-                val longValue = toWrite.toLong() + maxBit
-                val substring = longValue.toString(2).substring(1).mask(mask)
-                memory[memAddress.toInt()] = substring
-            }
-        }
-        val sum = memory.values.map { it.toLong(2) }.sum()
-        println(memory)
-        println(sum)
-    }
-
-    fun String.mask(mask: String) : String {
-        val newval = CharArray(36)
-        for(i in 0 until this.length) {
-            val c = mask[i]
-            if(c == 'X') {
-                newval[i] = this[i]
-            } else {
-                newval[i] = mask[i]
-            }
-        }
-        return newval.joinToString(separator = "")
+        execute(memory1, this::writeToMem1)
     }
 
     override fun executeGoal_2() {
-        val memory = mutableMapOf<Long, String>()
+        execute(memory2, this::writeToMem2)
+    }
+
+    private fun execute(memory: MutableMap<Long, String>, writeFunction: KFunction0<Unit>) {
         allLines.forEach {
             val maskMatch = maskRegex.matches(it)
-            val memMatch = memRegex.matches(it)
-
             if (maskMatch) {
                 mask = maskRegex.find(it)!!.groupValues[1]
             } else {
                 val memValues = memRegex.find(it)!!.groupValues
-                val memAddress = memValues[1]
-                val toWrite = memValues[2]
+                memAddress = memValues[1]
+                toWrite = memValues[2]
 
-                val memVal = memAddress.toLong() + maxBit
-                val decode = memVal.toString(2).substring(1).decode(mask)
-
-                decode.forEach { memory[it] = toWrite }
-
+                writeFunction.call()
             }
         }
-        val sum = memory.values.map { it.toLong() }.sum()
-        println(memory)
-        println(sum)
+        println(memory.values.map { it.toLong(2) }.sum())
+    }
+
+    fun writeToMem1() {
+        val longValue = toWrite.toLong() + maxBit
+        memory1[memAddress.toLong()] = longValue.toString(2).substring(1).mask(mask)
+    }
+
+    fun String.mask(mask: String): String {
+        val newval = CharArray(36)
+        this.forEachIndexed { i, c ->
+            when (mask[i]) {
+                'X' -> newval[i] = c
+                else -> newval[i] = mask[i]
+            }
+        }
+        return newval.joinToString("")
+    }
+
+    fun writeToMem2() {
+        val memVal = memAddress.toLong() + maxBit
+        val decode = memVal.toString(2).substring(1).decode(mask)
+        decode.forEach { memory2[it] = toWrite.toLong().toString(2) }
     }
 
     fun String.decode(mask: String): List<Long> {
         val addresses = mutableListOf<CharArray>()
-        if(addresses.isEmpty()) {
-            addresses.add(CharArray(36){'0'})
-        }
+        addresses.add(CharArray(36) { '0' })
         mask.forEachIndexed { i, c ->
-            if(c == 'X') {
-                val toAdd = mutableListOf<CharArray>()
-                addresses.forEach {
-                    val copyAddress = it.copyOf()
-                    copyAddress[i] = '1'
-                    toAdd.add(copyAddress)
-                    it[i] = '0'
+            when (c) {
+                'X' -> {
+                    val toAdd = mutableListOf<CharArray>()
+                    addresses.forEach {
+                        val copyAddress = it.copyOf()
+                        copyAddress[i] = '1'
+                        toAdd.add(copyAddress)
+                        it[i] = '0'
+                    }
+                    addresses.addAll(toAdd)
                 }
-                addresses.addAll(toAdd)
-            } else {
-                addresses.forEach { it[i] = if(c == '1') '1' else this[i] }
+                else -> addresses.forEach { it[i] = if (c == '1') '1' else this[i] }
             }
         }
-        return addresses.map { it.joinToString("").trim().toLong(2) }
+        return addresses.map { it.joinToString("").toLong(2) }
     }
 }
