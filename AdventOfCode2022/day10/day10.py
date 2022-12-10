f79225f459965c6ca1f67b860c6a0ffc6bd0b1b6
isor91 <ratkovszki.istvn@gmail.com>
2022-12-10 11:55:01 +0100

    Day10 2022 update

--- AdventOfCode2022/day10/day10.py
+++ AdventOfCode2022/day10/day10.py
@@ -7,42 +7,26 @@ instructions = {
     'addx': {
         'timeout': 2,
         'args': 1,
-        'action': lambda x, a: x+sum(a)
+        'action': lambda x, a: x+a
     }
 }
 
-interesting_cycles = [i for i in range(20,221,40)]
-interesting_cycle_values = []
-print(interesting_cycles)
-i = 0
 cycle = 0
 x = 1
+
+values = []
 with open('day10') as input:
     commands = [l.strip() for l in input]
     for c in commands:
-        args = [c.split()[i] for i in range(len(c.split()))]
+        args = c.split()
         instruction = instructions[args[0]]
-
         for j in range(instruction['timeout']):
-            current_pixel = cycle % 40
-            if(current_pixel in [x-1, x, x+1]):
-                print('#', end = '')
-            else:
-                print('.', end = '')
+            values.append(x)
             cycle +=1
-            if(cycle % 40 == 39):
-                print()
+        x = instruction['action'](x, int(args[1]) if len(args) == 2 else 0)
 
+    interesting_cycles = [i * values[i-1] for i in range(20,221,40)]
+    print(sum(interesting_cycles))
 
-
-    #     cycle += instruction['timeout']
-    #     if( i < len(interesting_cycles) and cycle+1 > interesting_cycles[i]):
-    #         new_val = interesting_cycles[i] * x
-    #         interesting_cycle_values.append(new_val)
-    #         print(interesting_cycles[i], x, new_val, sum(interesting_cycle_values))
-    #         i+=1
-        arguments = list(map(int,(args[i+1] for i in range(instruction['args']))))
-        x = instruction['action'](x, arguments)
-    #     # print(cycle, c, x)
-
-    # print(interesting_cycle_values, sum(interesting_cycle_values))
+    rows = [[x for x in values[i:i+40]] for i in range(0, len(values), 40)]
+    print('\n'.join([''.join(['%' if p in [r[p] - 1, r[p], r[p] + 1] else ' '  for p in range(len(r))]) for r in rows]))
