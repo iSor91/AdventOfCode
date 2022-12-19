169fbe2c66fd66107285588c1d5730da813ff248
isor91 <ratkovszki.istvn@gmail.com>
2022-12-19 13:48:22 +0100

    Adds task description

---
+++ AdventOfCode2022/day19/day19.py
@@ -0,0 +1,14 @@
+# i have one ore-collecting robot -> need ore for any robot
+# need clay collecting robot
+# need obsidian collecting robot -> waterproof with clay
+# need geode-cracking robot from obsidian
+
+# every robot collects its resource type 1/min
+# robot factory produces robots 1/min
+#   consumes the requested resources
+
+# i have blueprints, need to figure out which is the best
+# what is the max output of opened geodes after 24 minutes?
+
+# calculate the quality level of each blueprint : blueprint id * max opened geodes
+# sum up all the blueprint quality levels
