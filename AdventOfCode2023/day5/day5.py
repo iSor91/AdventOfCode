a4a70676eac754a706e94da84dbceefedf74b857
isor91 <ratkovszki.istvn@gmail.com>
2023-12-05 10:43:47 +0100

    Adds solution for 2023 day5 2

--- AdventOfCode2023/day5/day5.py
+++ AdventOfCode2023/day5/day5.py
@@ -34,35 +34,49 @@ def sections(seed, dest):
 
     return result
 
-def update_mapping(source_touple, seed_range_map):
-    print(source_touple)
+def update_mapping(source_touple, seed_range_map, affected_ones):
     finished = False
     while(not finished):
         mapping_to_seed_range = {seed_range_map[s][-1]:s for s in seed_range_map}
         print()
+        print(source_touple)
         print(seed_range_map)
         print(f"recalculated: {mapping_to_seed_range}")
+        i=0
         for seed in mapping_to_seed_range:
+            i+=1
             seed_sects = (sections(seed, source_touple))
             affected = seed_sects[0]
-            if(len(affected) == 0):
+            if(len(affected) == 0 or affected[0][1] == 0):
+                print(f"{seed} no affected")
                 continue
-            print(f"{source_touple} affected {seed}")
-            history = seed_range_map.pop(mapping_to_seed_range[seed])
 
             diff = affected[0][0]-source_start
-            extended_history=[s for s in history]
-            extended_history.append((dest_start+diff, affected[0][1]))
-            seed_range_map[affected[0]]=extended_history
-            not_affected = seed_sects[1]
-            for not_a in not_affected:
-                seed_range_map[not_a]=history
-            if(len(not_affected) > 0 and len(affected) > 0): 
-                break
-        else:
-            finished = True
+            new_dest = (dest_start+diff, affected[0][1])
 
-with open('test') as input:
+            not_affected = seed_sects[1]
+            history = seed_range_map.pop(mapping_to_seed_range[seed])
+            extended_history=[s for s in history]
+            extended_history.append(new_dest)
+            affected_ones[new_dest]=extended_history
+
+            if(len(not_affected) == 0):
+                print(f"{seed} no not affected")
+                continue
+
+            print(f"splitting {seed} to {affected} and {not_affected}")
+
+            for not_a in not_affected :
+                if (not_a[1]!=0):
+                    extended_history2=[s for s in history]
+                    extended_history2.append(not_a)
+                    seed_range_map[not_a]=extended_history2
+            break
+        print(i, len(mapping_to_seed_range))
+        if(i >= (len(mapping_to_seed_range)-1)):
+            finished=True
+
+with open('data') as input:
     seeds = input.readline().strip()
     seed_list = list(map(lambda x: int(x), re.split(": | ", seeds)[1:]))
     seed_ranges = [(seed_list[i], seed_list[i+1]) for i in range(0,len(seed_list),2)]
@@ -72,6 +86,7 @@ with open('test') as input:
 
     mapping_to_seed = {}
     mapping_to_seed_range = {}
+    affected_ones={}
 
     for l in input.readlines():
         line=l.strip()
@@ -80,6 +95,9 @@ with open('test') as input:
 
         elif(not re.match(r"^\d+.*", l)):
             mapping_to_seed = {seed_map[s][-1]:s for s in seed_map}
+            for a in affected_ones:
+                seed_range_map[a]=affected_ones[a]
+            affected_ones={}
 
         else:
             [dest_start, source_start, range_len]=list(map(lambda x: int(x), re.split(" ", line)))
@@ -93,9 +111,12 @@ with open('test') as input:
             # print(dest_start, source_start, range_len)
 
             source_touple = (source_start, range_len)
-            update_mapping(source_touple, seed_range_map)
+            update_mapping(source_touple, seed_range_map, affected_ones)
             print(f"after: {seed_range_map}")
 
 
     print(min([seed_map[s][-1] for s in seed_map]))
-    print(seed_range_map)
+
+    for a in affected_ones:
+        seed_range_map[a]=affected_ones[a]
+    print(min(list(map(lambda x: x[0], [seed_range_map[s][-1] for s in seed_range_map]))))
