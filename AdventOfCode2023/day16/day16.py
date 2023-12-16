a023fcd52ec32b8df3c290feb1f6fc6bfe74bb5b
isor91 <ratkovszki.istvn@gmail.com>
2023-12-16 07:52:04 +0100

    Adds 2023 day16 solution part 2

--- AdventOfCode2023/day16/day16.py
+++ AdventOfCode2023/day16/day16.py
@@ -2,10 +2,6 @@
 active_objects={}
 [south, east]=[0,0]
 
-beams = [{"pos": (0,-1), "dir": (0,1), }]
-start = []
-start.append(((0,0), (0,1)))
-energized = set()
 
 def get_new_dir(dir, t):
     if(t == '-'):
@@ -18,7 +14,7 @@ def get_new_dir(dir, t):
         return [dir]
     return []
 
-def get_new_beam(beam, east, south):
+def get_new_beam(beam, east, south, start):
     new_pos = tuple(map(sum,zip(beam["pos"],beam["dir"])))
     if(new_pos[0] >= south or new_pos[1] >= east or new_pos[0] < 0 or new_pos[1] < 0):
         return []
@@ -40,27 +36,53 @@ def get_new_beam(beam, east, south):
                 new_beams.append({"pos": new_pos, "dir": d})
     return new_beams
 
-
-with open('data') as input:
-    lines=input.readlines()
-    active_objects = {(i,j):t for i,line in enumerate(lines) for j,t in enumerate([l for l in line.strip()]) if t!='.'}
-    south = len(lines)
-    east = len(lines[0].strip())
+def calculate(starting_beam):
+    beams = [starting_beam]
+    start = []
+    start.append(((0,0), (0,1)))
+    energized = set()
 
     while(len(beams) > 0):
         new_beams = []
         for b in beams:
             energized.add(b["pos"])
-            for new_beam in get_new_beam(b, east, south):
+            for new_beam in get_new_beam(b, east, south, start):
                 # print(beam)
                 new_beams.append(new_beam)
         beams = new_beams
-        print(len(beams))
+        # print(len(beams))
+
+    # for i in range(south):
+    #     for j in range(east):
+    #         print(active_objects[(i,j)] if (i,j) in active_objects else '#' if (i,j) in energized else ' ', end='')
+    #     print()
+
+    return len(energized) - 1
+
+with open('data') as input:
+
+
+    lines=input.readlines()
+    active_objects = {(i,j):t for i,line in enumerate(lines) for j,t in enumerate([l for l in line.strip()]) if t!='.'}
+    south = len(lines)
+    east = len(lines[0].strip())
+    max=0
+    cnt = 0
+
+    for i in range(east):
+        curr = calculate({"pos": (-1,i), "dir": (1,0)})
+        if(curr > max):
+            max = curr
+        curr = calculate({"pos": (south,i), "dir": (-1,0)})
+        if(curr > max):
+            max = curr
 
     for i in range(south):
-        for j in range(east):
-            print(active_objects[(i,j)] if (i,j) in active_objects else '#' if (i,j) in energized else ' ', end='')
-        print()
+        curr = calculate({"pos": (i,-1), "dir": (0,1)})
+        if(curr > max):
+            max = curr
+        curr = calculate({"pos": (i,east), "dir": (0,-1)})
+        if(curr > max):
+            max = curr
 
-    print(len(energized) - 1)
-
+    print(max)
