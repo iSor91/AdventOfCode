b79ee18b3a11752fd83ecf76b94da90af0579b53
isor91 <ratkovszki.istvn@gmail.com>
2023-12-20 09:39:15 +0100

    Adds 2023 day20 solution 2

--- AdventOfCode2023/day20/day20.py
+++ AdventOfCode2023/day20/day20.py
@@ -14,6 +14,9 @@
 
 #let high pulse be 1 and low pulse be -1
 
+from functools import reduce
+
+
 pulse_list = []
 
 module_map={}
@@ -24,10 +27,14 @@ conjunction_map={}
 sent_pulse_map = {-1: 0, 1: 0}
 state_map = {}
 rx_pulses = []
+change_map = {}
+
+cn_changes = {'sv':[], 'th':[], 'gh':[], 'ch':[]}
 
 def broadcast_pulse(caller, called, pulse):
     # print(f" {caller} {pulse} {called}")
     out_pulse = pulse
+    change_map[called].append({'cycle': cycle, 'pulse': out_pulse, 'step': step})
     for destination in module_map[called]['destination']:
         sent_pulse_map[out_pulse]+=1
         pulse_list.append((called, destination, pulse))
@@ -36,6 +43,7 @@ def switch_pulse(caller, called, pulse):
     # print(f" {caller} {pulse} {called}")
     if(pulse == -1):
         out_pulse = switch_map[called] * pulse
+        change_map[called].append({'cycle': cycle, 'pulse': out_pulse, 'step': step})
         switch_map[called] = out_pulse
         for destination in module_map[called]['destination']:
             sent_pulse_map[out_pulse]+=1
@@ -43,8 +51,12 @@ def switch_pulse(caller, called, pulse):
 
 def conjunction_pulse(caller, called, pulse):
     # print(f" {caller} {pulse} {called}")
+    prev_out = -1 if all(map(lambda x: x == 1, conjunction_map[called].values())) else 1
     conjunction_map[called][caller] = pulse
     out_pulse = -1 if all(map(lambda x: x == 1, conjunction_map[called].values())) else 1
+    if(called =='cn' and pulse == 1):
+        cn_changes[caller].append((cycle, step, caller, conjunction_map[called], out_pulse))
+    change_map[called].append({'cycle': cycle, 'pulse': out_pulse, 'step': step})
     for destination in module_map[called]['destination']:
         sent_pulse_map[out_pulse]+=1
         pulse_list.append((called, destination, out_pulse))
@@ -79,6 +91,7 @@ with open ('data') as asdf:
                 'action': action
             }
         module_map[module['name']]=module
+        change_map[module['name']]=[]
 
     for module in module_map.values():
         if (module['full_name'].startswith('&')):
@@ -88,46 +101,29 @@ with open ('data') as asdf:
             if(dest in conjunction_map):
                 conjunction_map[dest][module['name']]=-1
 
-    # print(module_map)
-    # print(conjunction_map)
-
 state_id = f"{str(switch_map)}{str(conjunction_map)}"
-cycle = 0
-# while(state_id not in state_map and cycle < 1000):
-while(len(rx_pulses) != 1):
+cycle = 1
+step = 0
+while (not all([len(x) > 0 for x in [cn_changes[y] for y in cn_changes]])):
     rx_pulses=[]
     pulse_list.append(('button', 'broadcaster', -1))
     sent_pulse_map[-1]+=1
     while(len(pulse_list) != 0):
+        step+=1
         current_pulse = pulse_list[0]
         pulse_list=pulse_list[1:]
         if(current_pulse[1] in module_map):
             module_map[current_pulse[1]]['action'](*current_pulse)
 
-    # state_map[state_id]=sent_pulse_map
-    sent_pulse_map={-1: 0, 1: 0}
+    if(cycle == 1000):
+        print(sent_pulse_map[-1], sent_pulse_map[1], sent_pulse_map[-1]*sent_pulse_map[1])
+
+    state_map[state_id]=sent_pulse_map
     # print(state_map)
     state_id = f"{str(switch_map)}{str(conjunction_map)}"
     cycle+=1
-    print(cycle, rx_pulses)
 
-full_cycles = int(1000/len(state_map))
-remainder = 1000%len(state_map)
-repeat_keys = [(i,x) for i,x in enumerate(state_map.keys()) if i < remainder]
 
-print(full_cycles, remainder, repeat_keys)
-
-sent_high_pulses_fc = [state_map[s][1] * full_cycles for s in state_map]
-sent_low_pulses_fc = [state_map[s][-1] * full_cycles for s in state_map]
-
-sent_high_pulses_rem = [state_map[s][1] for s in state_map if s in repeat_keys]
-sent_low_pulses_rem = [state_map[s][-1] for s in state_map if s in repeat_keys]
-
-print(sent_high_pulses_fc, sent_high_pulses_rem, sent_low_pulses_fc, sent_low_pulses_rem)
-
-high=sum(sent_high_pulses_fc) + sum(sent_high_pulses_rem)
-low=sum(sent_low_pulses_fc) + sum(sent_low_pulses_rem)
-
-print(high, low, high*low)
-
-print(cycle)
+for c in cn_changes:
+    print(c, cn_changes[c])
+print(reduce(lambda acc, a: acc * int(a), [cn_changes[c][0][0] for c in cn_changes], 1))
